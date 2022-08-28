d82983e878a218ff7fef28f5b878af9aca83a3ef
neshka <agacode0@gmail.com>
2022-08-28 20:48:13 +0100

    add config, joi, debuger

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const app = express();
+const Joi = require("joi");
+const config = require("config");
+const startupdebugger = require("debug")("app");
 
+const logger = require("./logger.js");
+const app = express();
 //enable parsing up JSON obj in a body of the req
 app.use(express.json());
+app.use(logger);
+
+//Configureation
+console.log(`Application Name: ${config.get("name")}`);
+console.log(`Email: ${config.get("mail.host")}`);
+console.log(`Password: ${config.get("mail.password")}`);
+
+if (app.get("env") === "development") {
+  startupdebugger("dev env");
+}
 
 const port = process.env.PORT || 3000;
 const users = [
@@ -13,8 +27,8 @@ const users = [
 
 app.get("/api/users/:id", (req, res) => {
   const user = users.find((u) => u.id === parseInt(req.params.id));
-  if (!user) res.send("User not found").status(404);
-  res.send(req.query);
+  if (!user) return res.status(404).send("User not found");
+  res.send(user);
 });
 
 app.get("/api/users", (req, res) => {
@@ -22,14 +36,45 @@ app.get("/api/users", (req, res) => {
 });
 
 app.post("/api/users", (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = {
     id: users.length + 1,
     name: req.body.name,
   };
+
   users.push(user);
+  res.send(user);
+});
+
+app.put("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send("User not found");
+
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  user.name = req.body.name;
+  res.send(user);
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send("User not found");
+
+  const index = users.indexOf(user);
+  users.splice(index, 1);
   res.send(user);
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(user);
+}
